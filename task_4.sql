70545fd5b44cf98ceb098c4675ea23943d27dbd8
Tobi <markinstobi@gmail.com>
2024-07-26 21:58:31 +0100

    week 8

--- task_4.sql
+++ task_4.sql
@@ -61,4 +61,4 @@ mycursor.execute("""
 """
 )
 
-SHOW COLUMNS FROM student;
+mycursor.execute("INFORMATION_SCHEMA.COLUMNS", "SELECT", "COLUMN_NAME", "COLUMN_TYPE", "TABLE_SCHEMA = 'alx_book_store'", "TABLE_NAME = 'Books')
