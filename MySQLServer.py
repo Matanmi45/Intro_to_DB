3d53458005f2aa377b9409b3efba4e881b77742d
Tobi <markinstobi@gmail.com>
2024-07-26 11:34:09 +0100

    week 8

--- MySQLServer.py
+++ MySQLServer.py
@@ -1,18 +1,12 @@
-from getpass import getpass
-from mysql.connector import connect, Error
+import mysql.connector
+mydb = CREATE DATABASE alx_book_store;
 
-connection = try:
-    with connect(
-        host="localhost",
-        user=input("Enter username: "),
-        password=getpass("Enter password: "),
-    ) as connection:
-        print(“Database 'alx_book_store' created successfully!”)
-except Error as e:
-    print(e)
-
-alx_book_store = CREATE DATABASE alx_book_store;
-cursor = connection.cursor()
-cursor.execute(alx_book_store)
-cursor.close()
-connection.close()
+mydb = try:
+          mysql.connector.connect(
+          host="localhost",
+          user="yourusername",
+          password="yourpassword",
+          database="mydb")
+          print("Database 'alx_book_store' created successfully!")
+       except:
+          print("connection not established")
