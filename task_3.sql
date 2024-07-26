d8c795f87cef8f4a7bad54afeade25553711ce7d
Tobi <markinstobi@gmail.com>
2024-07-26 12:05:58 +0100

    week 8

---
+++ task_3.sql
@@ -0,0 +1,69 @@
+import mysql.connector
+mydb = CREATE DATABASE IF NOT EXISTS alx_book_store;
+
+mydb = try:
+          mysql.connector.connect(
+          host="localhost",
+          user="yourusername",
+          password="yourpassword",
+          database="mydb")
+          print("Database 'alx_book_store' created successfully!")
+       except:
+          print("connection not established
+
+mycursor = mydb.cursor()
+          
+mycursor.execute("""
+   CREATE TABLE IF NOT EXISTS Books (
+   book_id INT PRIMARY KEY
+   title VARCHAR(130)
+   FOREIGN KEY (author_id) REFERENCES Authors(author_id) 
+   price DOUBLE
+   publication_date DATE
+);
+"""
+)
+
+mycursor.execute("""
+   CREATE TABLE IF NOT EXISTS Authors (
+   author_id INT PRIMARY KEY
+   author_name VARCHAR(215)
+);
+"""
+)
+
+mycursor.execute("""
+   CREATE TABLE IF NOT EXISTS Customers (
+   customer_id INT PRIMARY KEY
+   customer_name VARCHAR(215)
+   email VARCHAR(215)
+   address TEXT
+);
+"""
+)
+
+mycursor.execute("""
+  CREATE TABLE IF NOT EXISTS Orders(
+  order_id INT PRIMARY KEY
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
+  order_date DATE
+);
+"""
+)
+
+mycursor.execute("""
+  CREATE TABLE IF NOT EXISTS Order_Details(
+   orderdetail_id INT PRIMARY KEY
+   FOREIGN KEY (order_id) REFERENCES Orders (order_id)
+   FOREIGN KEY (book_id) REFERENCES Books(book_id)
+   quantity DOUBLE
+);
+"""
+)
+
+mycursor.execute("SHOW TABLES")
+
+for table in mycursor:
+    print(table)
+
+
